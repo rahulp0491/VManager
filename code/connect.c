6e5a7d7e2900fe2da48c0136783b94b8bb550191
Rahul P <rahulp0491@gmail.com>
2012-02-13 03:46:30 +0530

    Added code to connec to local hypervisor

---
+++ code/connect.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libvirt/libvirt.h>			// INCLUDING THE LIBVIRT LIBRARY
+
+/* -------------------------------------ERROR LOG ------------------------------------------------*/
+void error (char *errMsg) {
+	fprintf (stderr, "ERROR: %s\n", errMsg);
+	exit(1);
+}
+
+/* ---------------------------------------MAIN------------------------------------------------------ */
+int main (int argc, char *argv[]) {
+	virConnectPtr conn;						// CREATING AN OBJECT USING virConnectPtr HANDLER
+	conn = virConnectOpen ("Xen:///system");	// CONNECTING TO LOCAL XEN HYPERVISOR 
+	if (conn == NULL)
+		error ("Failed to open connection to Xen:///system");
+	
+	virConnectClose (conn);					// CLOSING CONNECTION
+	return 0;
+}
+
