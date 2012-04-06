caf0c2b457e700f162384c809f356651acc4579b
Rahul P <rahulp0491@gmail.com>
2012-04-06 16:06:25 +0530

    Added main file

---
+++ code/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <libvirt/libvirt.h>			// INCLUDING THE LIBVIRT LIBRARY
+#include <pthread.h>
+#include <assert.h>
+#include <string.h>
+#include "handler.h"
+#include "definitions.h"
+
+
+/* -MAIN- */
+int main (int argc, char *argv[]) {
+	char input [100];
+	int inputNum;
+	while (1) {
+		printf ("VManager-># ");
+		scanf("%s", input);
+		if (!strcmp (input, "quit")) 
+			return 0;
+		inputNum = assignNum (input);
+		handleInput (inputNum);
+	}
+}
+
