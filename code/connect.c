42da835a21c172573aa98a903e9c179603221ef8
Rahul P <rahulp0491@gmail.com>
2012-04-06 10:27:20 +0530

    Additions to connect

--- code/connect.c
+++ code/connect.c
@@ -19,10 +19,17 @@ int main (int argc, char *argv[]) {
 	char *s;
 	s = virConnectGetCapabilities (conn);
 	printf ("Capabilities:\n%s\n", s);
+	
+	free(s);
+	s = virConnectGetHostname (conn);
+	printf ("Hostname: %s\n", s);
+	
+	
 	if (virConnectClose (conn) != 0) {
 		fprintf (stderr, "Cannot close connection to qemu:///session\n");
 		return -1;
 	}
+	
 	printf ("Connection to qemu:///session closed\n");
 	return 0;
 }
