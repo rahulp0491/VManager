7341298b706768a44905db75dfbf17bcc2b62d84
Rahul P <rahulp0491@gmail.com>
2012-04-07 16:45:59 +0530

    Added necessary function declarations

--- code/definitions.h
+++ code/definitions.h
@@ -3,7 +3,7 @@
 #define MaxFileName 100
 #define MaxNumConnections 10
 
-#define NumOfInputOptions 15
+#define NumOfInputOptions 17
 
 #define CONNECT 1
 #define CLOSECON 2
@@ -15,21 +15,26 @@
 #define RESTORE 8
 #define SHUTDOWN 9
 #define REBOOT 10
-#define DESTROY 11
+#define DOMINFO 11
 #define NUMDOMAIN 12
 #define NODEINFO 13
 #define NODELIST 14
 #define NODECAP 15
+#define LOAD 16
+#define DOMLIST 17
+
 
 int assignNum (char *input);
 int handleInput (int input);
 int getNextDomainThreadNum (int conNum);
 int createDomain (int conNum);
-int getNextDomain (int conNum);
 void *manageDomain (void *arg);
 int createConnection (int conNum);
-void *manageConnections (void *arg);
 int isConnectionEstablished (char *hostname);
 int getNextConnThread ();
 void printNodeInfo (virNodeInfo nodeinfo);
 void printNodeList ();
+int connectionWithSameURI (char *uri);
+void printDomInfo (virDomainInfoPtr dominfo);
+int isDomCreated (char *domName, int conNum);
+void printDomList (int conNum);
